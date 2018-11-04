7e0156c51500170214fd03984b2a0c2da0fae6f1
darken <darken@darken.eu>
2018-11-04 19:28:11 +0100

    Some refactoring of generator hooks

--- backend/src/main/java/eu/darken/backend/webserver/graphql/extensions/CustomSchemaGeneratorHooks.kt
+++ backend/src/main/java/eu/darken/backend/webserver/graphql/extensions/CustomSchemaGeneratorHooks.kt
@@ -1,7 +1,7 @@
 package eu.darken.backend.webserver.graphql.extensions
 
 import com.expedia.graphql.schema.hooks.NoopSchemaGeneratorHooks
-import graphql.schema.*
+import graphql.schema.GraphQLType
 import java.time.Instant
 import javax.inject.Inject
 import kotlin.reflect.KClass
@@ -11,31 +11,9 @@ class CustomSchemaGeneratorHooks @Inject constructor() : NoopSchemaGeneratorHook
 
     override fun willGenerateGraphQLType(type: KType): GraphQLType? {
         return when (type.classifier as? KClass<*>) {
-            Instant::class -> typeInstant
+            Instant::class -> InstantCoercing.type
             else -> null
         }
     }
 }
 
-internal val typeInstant = GraphQLScalarType("Instant", "A ISO-8601 formatted java.time.Instant", InstantCoercing)
-
-private object InstantCoercing : Coercing<Instant, String> {
-    override fun parseValue(input: Any?): Instant = try {
-        Instant.parse(serialize(input))
-    } catch (e: Exception) {
-        throw CoercingParseValueException(e)
-    }
-
-    override fun parseLiteral(input: Any?): Instant? = try {
-        val isoString = (input as? Instant)?.toString()
-        Instant.parse(isoString)
-    } catch (e: Exception) {
-        throw CoercingParseLiteralException(e)
-    }
-
-    override fun serialize(fetchedResult: Any?): String = try {
-        fetchedResult.toString()
-    } catch (e: Exception) {
-        throw CoercingSerializeException(e)
-    }
-}
